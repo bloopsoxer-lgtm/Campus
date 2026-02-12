67a2c1dd551eb625b22ccfb59f7fc22eb733ba82
Jonas <you@example.com>
2026-02-12 05:43:20 -0800

    **Campus**
    Adcionei duas rotas, room.html e config.html

--- home/home.js
+++ home/home.js
@@ -2,16 +2,24 @@ const chat = document.getElementById("chat")
 const profile = document.getElementById("profile")
 const config = document.getElementById("config")
 const info = document.getElementById("info")
+const room = document.getElementById("room")
 
 chat.addEventListener("click", function() {
     window.location.href = "/home/chat.html";
 });
+
 profile.addEventListener("click", function() {
     window.location.href = "/home/profile.html";
 });
+
 config.addEventListener("click", function() {
     window.location.href = "/home/config.html";
 });
+
 info.addEventListener("click", function() {
     window.location.href = "/home/about.html";
+});
+
+room.addEventListener("click", function() {
+    window.location.href = "/home/room.html";
 });
